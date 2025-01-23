9400b8fd96a2594b213471e8341c860c00445faf
Xerx81 <ryuvraj832@gmail.com>
2025-01-23 10:14:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Little Lemon</h1>
+            <p>Your frontend is ready to build!</p>
+        </div>
+  );
+}
+
+export default App;
