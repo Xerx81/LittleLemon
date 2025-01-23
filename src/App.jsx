ab3d2ac96510952c46887c4aa99145d774f65ce4
Xerx81 <ryuvraj832@gmail.com>
2025-01-23 13:54:53 +0530

    Completed home and about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                 </Routes>
+                <Footer />
             </Router>
         </>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,10 @@ function Header() {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
+                    <li><Link to="/menu">Menu</Link></li>
+                    <li><Link to="/book">Book</Link></li>
                 </ul>
             </nav>
-            <h1>Welcome To Little Lemon</h1>
         </>
     );
 }
